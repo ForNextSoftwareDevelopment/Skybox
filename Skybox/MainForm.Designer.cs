5ffb90ccdd4f75d7c5afa7158edf5805320a501f
Dirk Prins <info@fornext.nl>
2019-05-07 09:01:08 +0200

    Add files via upload

---
+++ Skybox/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Skybox
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblOffsetX = new System.Windows.Forms.Label();
+            this.numericUpDownOffsetX = new System.Windows.Forms.NumericUpDown();
+            this.lblOffsetY = new System.Windows.Forms.Label();
+            this.numericUpDownOffsetY = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOffsetX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOffsetY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 13);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(93, 13);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblOffsetX
+            // 
+            this.lblOffsetX.AutoSize = true;
+            this.lblOffsetX.Location = new System.Drawing.Point(572, 18);
+            this.lblOffsetX.Name = "lblOffsetX";
+            this.lblOffsetX.Size = new System.Drawing.Size(45, 13);
+            this.lblOffsetX.TabIndex = 6;
+            this.lblOffsetX.Text = "OffsetX:";
+            // 
+            // numericUpDownOffsetX
+            // 
+            this.numericUpDownOffsetX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownOffsetX.Location = new System.Drawing.Point(623, 16);
+            this.numericUpDownOffsetX.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownOffsetX.Name = "numericUpDownOffsetX";
+            this.numericUpDownOffsetX.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDownOffsetX.TabIndex = 5;
+            this.numericUpDownOffsetX.ValueChanged += new System.EventHandler(this.NumericUpDownOffsetX_ValueChanged);
+            // 
+            // lblOffsetY
+            // 
+            this.lblOffsetY.AutoSize = true;
+            this.lblOffsetY.Location = new System.Drawing.Point(694, 18);
+            this.lblOffsetY.Name = "lblOffsetY";
+            this.lblOffsetY.Size = new System.Drawing.Size(45, 13);
+            this.lblOffsetY.TabIndex = 8;
+            this.lblOffsetY.Text = "OffsetY:";
+            // 
+            // numericUpDownOffsetY
+            // 
+            this.numericUpDownOffsetY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownOffsetY.Location = new System.Drawing.Point(745, 16);
+            this.numericUpDownOffsetY.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownOffsetY.Name = "numericUpDownOffsetY";
+            this.numericUpDownOffsetY.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDownOffsetY.TabIndex = 7;
+            this.numericUpDownOffsetY.ValueChanged += new System.EventHandler(this.NumericUpDownOffsetY_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 661);
+            this.Controls.Add(this.lblOffsetY);
+            this.Controls.Add(this.numericUpDownOffsetY);
+            this.Controls.Add(this.lblOffsetX);
+            this.Controls.Add(this.numericUpDownOffsetX);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.pictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(840, 700);
+            this.MinimumSize = new System.Drawing.Size(840, 700);
+            this.Name = "MainForm";
+            this.Text = "Skybox Textures";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOffsetX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownOffsetY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblOffsetX;
+        private System.Windows.Forms.NumericUpDown numericUpDownOffsetX;
+        private System.Windows.Forms.Label lblOffsetY;
+        private System.Windows.Forms.NumericUpDown numericUpDownOffsetY;
+    }
+}
+
---
+++ Skybox/MainForm.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Skybox
+{
+    public partial class MainForm : Form
+    {
+        private string folderPath;
+        private string filePath;
+        private Image orgImage = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures).ToString();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (orgImage != null)
+            {
+                try
+                { 
+                    Bitmap bmSkyLeft = new Bitmap(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3);
+                    Bitmap bmSkyRight = new Bitmap(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3);
+                    Bitmap bmSkyTop = new Bitmap(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3);
+                    Bitmap bmSkyBottom = new Bitmap(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3);
+                    Bitmap bmSkyFront = new Bitmap(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3);
+                    Bitmap bmSkyBack = new Bitmap(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3);
+                    Rectangle dstRect = new Rectangle(new Point(0, 0), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+
+                    using (Graphics gr = Graphics.FromImage(bmSkyLeft))
+                    {
+                        Rectangle srcRect = new Rectangle(new Point(0, pictureBox.Image.Height / 3), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+                        gr.DrawImage(orgImage, dstRect, srcRect, GraphicsUnit.Pixel);
+
+                        bmSkyLeft.Save(folderPath + "/skyleft.bmp", ImageFormat.Bmp);
+                    }
+
+                    using (Graphics gr = Graphics.FromImage(bmSkyRight))
+                    {
+                        Rectangle srcRect = new Rectangle(new Point(2 * pictureBox.Image.Width / 4, pictureBox.Image.Height / 3), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+                        gr.DrawImage(orgImage, dstRect, srcRect, GraphicsUnit.Pixel);
+
+                        bmSkyRight.Save(folderPath + "/skyright.bmp", ImageFormat.Bmp);
+                    }
+
+                    using (Graphics gr = Graphics.FromImage(bmSkyTop))
+                    {
+                        Rectangle srcRect = new Rectangle(new Point(pictureBox.Image.Width / 4, 0), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+                        gr.DrawImage(orgImage, dstRect, srcRect, GraphicsUnit.Pixel);
+
+                        bmSkyTop.Save(folderPath + "/skytop.bmp", ImageFormat.Bmp);
+                    }
+
+                    using (Graphics gr = Graphics.FromImage(bmSkyBottom))
+                    {
+                        Rectangle srcRect = new Rectangle(new Point(pictureBox.Image.Width / 4, 2 * pictureBox.Image.Height / 3), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+                        gr.DrawImage(orgImage, dstRect, srcRect, GraphicsUnit.Pixel);
+
+                        bmSkyBottom.Save(folderPath + "/skybottom.bmp", ImageFormat.Bmp);
+                    }
+
+                    using (Graphics gr = Graphics.FromImage(bmSkyFront))
+                    {
+                        Rectangle srcRect = new Rectangle(new Point(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+                        gr.DrawImage(orgImage, dstRect, srcRect, GraphicsUnit.Pixel);
+
+                        bmSkyFront.Save(folderPath + "/skyfront.bmp", ImageFormat.Bmp);
+                    }
+
+                    using (Graphics gr = Graphics.FromImage(bmSkyBack))
+                    {
+                        Rectangle srcRect = new Rectangle(new Point(3 * pictureBox.Image.Width / 4, pictureBox.Image.Height / 3), new Size(pictureBox.Image.Width / 4, pictureBox.Image.Height / 3));
+                        gr.DrawImage(orgImage, dstRect, srcRect, GraphicsUnit.Pixel);
+
+                        bmSkyBack.Save(folderPath + "/skyback.bmp", ImageFormat.Bmp);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, "Can't save images: " + ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                MessageBox.Show(this, "Images saved in: " + folderPath, "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = folderPath; 
+            openFileDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)| *.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
+                folderPath = Path.GetDirectoryName(filePath);
+
+                try
+                {
+                    Image image = new Bitmap(filePath);
+                    orgImage = new Bitmap(image);
+                    pictureBox.Image = image;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(this, "This file is not a (readable) image: " + ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                numericUpDownOffsetX.Value = 0;
+                numericUpDownOffsetY.Value = 0;
+
+                DrawLines();
+            }
+        }
+
+        private void NumericUpDownOffsetX_ValueChanged(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(filePath);
+
+            int offsetX = Convert.ToInt32(numericUpDownOffsetX.Value);
+            int offsetY = Convert.ToInt32(numericUpDownOffsetY.Value);
+
+            Rectangle rect;
+            if (offsetX >= 0)
+            {
+                if (offsetY >= 0)
+                {
+                    rect = new Rectangle(offsetX, offsetY, bitmap.Width - offsetX, bitmap.Height - offsetY);
+                } else
+                {
+                    rect = new Rectangle(offsetX, 0, bitmap.Width - offsetX, bitmap.Height + offsetY);
+                }
+            }
+            else
+            {
+                if (offsetY >= 0)
+                {
+                    rect = new Rectangle(0, offsetY, bitmap.Width + offsetX, bitmap.Height - offsetY);
+                }
+                {
+                    rect = new Rectangle(0, 0, bitmap.Width + offsetX, bitmap.Height + offsetY);
+                }
+            }
+
+            orgImage = bitmap.Clone(rect, 0);
+
+            Image image = new Bitmap(orgImage);
+            pictureBox.Image = image;
+
+            DrawLines();
+        }
+
+        private void NumericUpDownOffsetY_ValueChanged(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(filePath);
+
+            int offsetX = Convert.ToInt32(numericUpDownOffsetX.Value);
+            int offsetY = Convert.ToInt32(numericUpDownOffsetY.Value);
+
+            Rectangle rect;
+            if (offsetX >= 0)
+            {
+                if (offsetY >= 0)
+                {
+                    rect = new Rectangle(offsetX, offsetY, bitmap.Width - offsetX, bitmap.Height - offsetY);
+                }
+                else
+                {
+                    rect = new Rectangle(offsetX, 0, bitmap.Width - offsetX, bitmap.Height + offsetY);
+                }
+            }
+            else
+            {
+                if (offsetY >= 0)
+                {
+                    rect = new Rectangle(0, offsetY, bitmap.Width + offsetX, bitmap.Height - offsetY);
+                }
+                {
+                    rect = new Rectangle(0, 0, bitmap.Width + offsetX, bitmap.Height + offsetY);
+                }
+            }
+
+            orgImage = bitmap.Clone(rect, 0);
+
+            Image image = new Bitmap(orgImage);
+            pictureBox.Image = image;
+
+            DrawLines();
+        }
+
+        void DrawLines()
+        {
+            int offsetX = Convert.ToInt32(numericUpDownOffsetX.Value);
+            int offsetY = Convert.ToInt32(numericUpDownOffsetY.Value);
+
+            int width = pictureBox.Image.Width;
+            int height = pictureBox.Image.Height;
+
+            Graphics g = Graphics.FromImage(pictureBox.Image);
+            Pen pen = new Pen(Color.Red);
+            g.DrawLine(pen, 0, height / 3, width, height / 3);
+            g.DrawLine(pen, 0, 2 * height / 3, width, 2 * height / 3);
+
+            g.DrawLine(pen, width / 4, 0, width / 4, height);
+            g.DrawLine(pen, 2 * width / 4, 0, 2 * width / 4, height);
+            g.DrawLine(pen, 3 * width / 4, height / 3, 3 * width / 4, 2 * height / 3);
+
+            g.Dispose();
+        }
+    }
+}
